6704c689c48fe102f871e2e58c515b984c6be813
Bondit333 <132305018+Bondit333@users.noreply.github.com>
2023-05-03 15:13:26 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from bson.binary import Binary
 import os
 
-def process_form_data(name, username, college, images, caption):
+def process_form_data(name, username, college, images, caption, verification):
     MONGO_URI = "mongodb+srv://bonditcommunities:4vvwgomYBtMSkwkE@cluster0.rrr56f3.mongodb.net/?retryWrites=true&w=majority"
 
     client = MongoClient(MONGO_URI)
@@ -21,31 +21,30 @@ def process_form_data(name, username, college, images, caption):
         "username": username,
         "college": college,
         "caption": caption,
+        "verification": verification,
         "images": image_binaries
     }
 
     collection.insert_one(document)
 
 def main():
-    st.title("College Post Submission")
-
+    st.title("University Post Submission")
     colleges = ["larmar2027"]
 
     name = st.text_input("Name")
     username = st.text_input("Username")
     college = st.selectbox("Select your college", options=colleges)
-    caption = st.text_area("Post caption")
+    caption =  st.text_area("Post caption")
+    verification = st.text_area("Paste in your college acceptance email for cornfirmation.")
     images = st.file_uploader("Upload images", type=['png', 'jpg', 'jpeg'], accept_multiple_files=True)
 
     if st.button("Post"):
-        if name and username and college and images and caption:
-            process_form_data(name, username, college, images, caption)
-            succ_msg = st.success("Form submitted successfully!")
+        if name and username and college and images and caption and verification:
+            process_form_data(name, username, college, images, caption, verification)
+            st.success("Post submitted. Expect to see yout post in 24 hours!")
         else:
             st.error("Please fill in all the fields and upload at least one image.")
-    
-    if (succ_msg):
-        st.write(10)
+ 
 
 if __name__ == "__main__":
     main()
