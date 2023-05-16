b487e5b358eb29ee9099f942908830e2d09d39fd
Bondit333 <132305018+Bondit333@users.noreply.github.com>
2023-05-15 23:47:08 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from bson.binary import Binary
 import os
 
-def process_form_data(name, username, college, images, caption, verification):
+def process_form_data(name, username, grad_year, college, images, caption, verification_email, verification_acceptance_letter):
     MONGO_URI = "mongodb+srv://bonditcommunities:4vvwgomYBtMSkwkE@cluster0.rrr56f3.mongodb.net/?retryWrites=true&w=majority"
 
     client = MongoClient(MONGO_URI)
@@ -15,13 +15,17 @@ def process_form_data(name, username, college, images, caption, verification):
         if image.name.endswith(('.png', '.jpg', '.jpeg')):
             image_binary = Binary(image.read())
             image_binaries.append(image_binary)
+    
+    
 
     document = {
         "name": name,
         "username": username,
+        "graduation_year": grad_year,
         "college": college,
         "caption": caption,
-        "verification": verification,
+        "verification": verification_email,
+        "acceptance_letter": Binary(verification_acceptance_letter.read()),
         "images": image_binaries
     }
 
@@ -33,14 +37,16 @@ def main():
 
     name = st.text_input("Name")
     username = st.text_input("Username")
+    grad_year = st.text_input("Graduation Year")
     college = st.selectbox("Select your college", options=colleges)
     caption =  st.text_area("Post caption")
-    verification = st.text_area("Paste in your college acceptance email for cornfirmation.")
+    verification_email = st.text_area("Paste in your college acceptance email for cornfirmation.")
+    verification_acceptance_letter = st.file_uploader("Upload college acceptance letter.", type=['.png'], accept_multiple_files=False)
     images = st.file_uploader("Upload images", type=['png', 'jpg', 'jpeg'], accept_multiple_files=True)
 
     if st.button("Post"):
-        if name and username and college and images and caption and verification:
-            process_form_data(name, username, college, images, caption, verification)
+        if name and username and college and images and caption and verification_email:
+            process_form_data(name, username, grad_year, college, images, caption, verification_email, verification_acceptance_letter)
             st.success("Post submitted. Expect to see yout post in 24 hours!")
         else:
             st.error("Please fill in all the fields and upload at least one image.")
